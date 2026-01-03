86cc3bd2387e638fb66687e01ac6702c8606e9d3
ehsansii <ehsansaravi@gmail.com>
2026-01-03 17:21:12 +0330

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.yourname.habitapp',
+  appId: 'com.vinasystem.adatyar',
   appName: 'Adat Yar',
   webDir: 'dist'
 };
