fffdf135aed9bec97d188541007acd079adae00e
ehsansii <ehsansaravi@gmail.com>
2026-01-03 10:21:47 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // این خط باعث می‌شود دکمه تغییر تم کار کند
   content: [
     "./index.html",
     "./index.tsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Vazirmatn', 'sans-serif'], // فونت وزیر را اینجا هم اضافه کردیم
+      },
+    },
   },
   plugins: [],
 }
