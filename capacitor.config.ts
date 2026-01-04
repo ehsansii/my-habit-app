673c212d558268d961e3bfac570c3f26a9f813fa
ehsansii <ehsansaravi@gmail.com>
2026-01-04 22:57:35 +0330

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.vinasystem.adatyar',
+  appId: 'com.ehsan.habitapp', // مطمئن شوید این همان نام پکیج شماست
   appName: 'Adat Yar',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      spinnerColor: "#4f46e5"
+    }
+  }
 };
 
 export default config;
